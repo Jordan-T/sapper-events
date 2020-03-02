1279a168ca263c254913c76aff5e380b74a1d09f
Jordan-T <jordan.taisne@gmail.com>
2020-03-02 12:57:53 +0100

    netlify auto build test

--- src/services/netlify.js
+++ src/services/netlify.js
@@ -1,7 +1,7 @@
 import { NETLIFY_BUILD_HOOK } from "../global";
 
 export function deploy() {
-  if (process.env.NEED_DEPLOY === "1") {
+  if (process.env.SAPPER_APP_NEED_DEPLOY === "1") {
     return fetch(NETLIFY_BUILD_HOOK, {method: "POST"});
   }
 }
